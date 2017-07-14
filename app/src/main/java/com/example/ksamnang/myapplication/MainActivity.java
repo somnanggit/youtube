9aaf3d630eef780504a1fb1f4dbf944f38d02833
somnang <somnanggit@gmail.com>
2017-07-14 11:30:50 -0700

    already test youtube

--- app/src/main/java/com/example/ksamnang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ksamnang/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.ksamnang.myapplication;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,16 +9,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
+   protected  WebView web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        web= (WebView) findViewById(R.id.web);
+        WebSettings wsetting=web.getSettings();
+        wsetting.setJavaScriptEnabled(true);
+        web.loadUrl("https://youtube.com");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-     WebView web;
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -37,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    web=(WebView)findViewById(R.id.webview);
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
